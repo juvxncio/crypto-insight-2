6af31e623941941dbbeaae86b46de5b46696a22b
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-15 17:08:02 -0300

    View atualizada com a requisição da api

--- crypto_insight/cryptos/views.py
+++ crypto_insight/cryptos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .coin_gecko import buscar_dados_criptomoedas
 
-# Create your views here.
+def listar_criptomoedas(request):
+    dados = buscar_dados_criptomoedas()
+    if dados is not None:
+        return JsonResponse(dados, safe=False)
+    else:
+        return JsonResponse({'erro': 'Falha ao buscar dados da API'}, status=500)
