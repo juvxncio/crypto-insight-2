b84c184f59e41639d902d1c1888ed402981dfcd5
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-15 17:02:39 -0300

    Condicional se status code 200 retorna dados json

--- crypto_insight/cryptos/coin_gecko.py
+++ crypto_insight/cryptos/coin_gecko.py
@@ -9,3 +9,10 @@ def buscar_dados_criptomoedas():
         'page': 1,
         'sparkline': False,
     }
+
+    resposta = requests.get(url_api, params=parametros)
+
+    if resposta.status_code == 200:
+        return resposta.json()
+    else:
+        return None
