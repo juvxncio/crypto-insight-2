6250bd6b386cec9f827cb891cacad5077a35741b
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-15 17:37:16 -0300

    requisição api feita

--- crypto_insight/cryptos/coin_gecko.py
+++ crypto_insight/cryptos/coin_gecko.py
@@ -1,11 +1,18 @@
 import requests
 
 def buscar_dados_criptomoedas():
-    url_api = 'https://api.coingecko.com/api/v3/coins/markets'
-    parametros = {
-        'vs_currency': 'brl',
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        'vs_currency': 'usd',
         'order': 'market_cap_desc',
         'per_page': 10,
         'page': 1,
-        'sparkline': False,
+        'sparkline': False
     }
+
+    resposta = requests.get(url, params=params)
+
+    if resposta.status_code == 200:
+        return resposta.json()
+    else:
+        return None
