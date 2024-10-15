ad1b9a9599a8c6d3d22620ed4c12bb882b03231b
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-15 14:53:47 -0300

    Criação de modelo e registro do app

--- crypto_insight/cryptos/models.py
+++ crypto_insight/cryptos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Crypto(models.Model):
+    nome = models.CharField(max_length=100)
+    simbolo = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=20, decimal_places=8)
+    volume = models.DecimalField(max_digits=20, decimal_places=8)
+
+    def __str__(self):
+        return f'{self.nome} ({self.simbolo})'
