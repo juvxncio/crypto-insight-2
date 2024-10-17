cc3e5e46833bdeb5d09b2cc718bec5c5d0289fe6
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-17 17:57:58 -0300

    ULSs de autenticação adicionadas

--- crypto_insight/crypto_insight/urls.py
+++ crypto_insight/crypto_insight/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cryptos.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
