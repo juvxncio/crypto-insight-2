24693b9c1ce92d5c836def87ec7c858bf9a09a88
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-10-17 17:02:42 -0300

    URLs atualizadas

--- crypto_insight/crypto_insight/urls.py
+++ crypto_insight/crypto_insight/urls.py
@@ -15,24 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cryptos.urls')),
-    path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-=======
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
->>>>>>> parent of ac5d77e (urls definidas)
 ]
