406f5f3cfa6376fe40a7dc5704f23d996f98b2be
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-06 15:10:12 -0300

    Implementação do logout

--- crypto_insight/cryptos/urls.py
+++ crypto_insight/cryptos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import listar_criptomoedas
+from . import views
 
 urlpatterns = [
     path('criptomoedas/', listar_criptomoedas, name='listar_criptomoedas'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- crypto_insight/cryptos/views.py
+++ crypto_insight/cryptos/views.py
@@ -3,6 +3,11 @@ from django.http import JsonResponse
 from .coin_gecko import buscar_dados_criptomoedas
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
 
 @permission_classes([IsAuthenticated])
 def listar_criptomoedas(request):
@@ -11,3 +16,10 @@ def listar_criptomoedas(request):
         return JsonResponse(dados, safe=False)
     else:
         return JsonResponse({'erro': 'Falha ao buscar dados da API'}, status=500)
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({"detail": "Logout realizado com sucesso!"}, status=status.HTTP_200_OK)
