1dde894fadce2da1c6f48567c0e8c1be614aeaa6
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-06 15:12:37 -0300

    Models favoritar moeda

--- crypto_insight/cryptos/models.py
+++ crypto_insight/cryptos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Crypto(models.Model):
     nome = models.CharField(max_length=100)
@@ -8,3 +9,15 @@ class Crypto(models.Model):
 
     def __str__(self):
         return f'{self.nome} ({self.simbolo})'
+    
+class Favorito(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    moeda_id = models.CharField(max_length=50)
+    nome_moeda = models.CharField(max_length=100)
+    simbolo = models.CharField(max_length=10)
+
+class Meta:
+    unique_together = ('usuario', 'moeda_id')
+
+def __str__(self):
+    return f"{self.usuario} - {self.nome_moeda}"
