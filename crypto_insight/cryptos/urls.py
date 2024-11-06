2ed80cb61b7b7c7ed8599a92adf2b50b22ffee96
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-06 15:17:11 -0300

    URLs para Favoritar e Remover Favoritos

--- crypto_insight/cryptos/urls.py
+++ crypto_insight/cryptos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import listar_criptomoedas
+from .views import listar_criptomoedas, FavoritarMoedaView, RemoverFavoritoView
 from . import views
 
 urlpatterns = [
     path('criptomoedas/', listar_criptomoedas, name='listar_criptomoedas'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('favoritar/', FavoritarMoedaView.as_view(), name='favoritar-moeda'),
+    path('remover-favorito/<str:moeda_id>/', RemoverFavoritoView.as_view(), name='remover-favorito'),
 ]
