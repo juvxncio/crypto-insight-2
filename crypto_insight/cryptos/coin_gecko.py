b746c4c7ac799bc26883129dc5f25ebd719b6d77
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-07 15:36:32 -0300

    Função que pega o histórico de preços

--- crypto_insight/cryptos/coin_gecko.py
+++ crypto_insight/cryptos/coin_gecko.py
@@ -22,7 +22,7 @@ class CoinGeckoAPI:
     def __init__(self):
         self.base_url = "https://api.coingecko.com/api/v3"
 
-    def get_coin_details(self, coin_id):
+    def get_detalhes_moedas(self, coin_id):
         url = f"{self.base_url}/coins/{coin_id}"
         params = {
             "localization": "false",
@@ -31,6 +31,20 @@ class CoinGeckoAPI:
             "community_data": "false",
             "developer_data": "false",
             "sparkline": "false"
+        }
+        response = requests.get(url, params=params)
+        return response.json() if response.status_code == 200 else None
+    
+    def get_historico_de_preco(self, coin_id, interval):
+        url = f"{self.base_url}/coins/{coin_id}/market_chart"
+        days = {
+            "1d": "1",
+            "7d": "7",
+            "1m": "30"
+        }.get(interval, "7")
+        params = {
+            "vs_currency": "brl",
+            "days": days
         }
         response = requests.get(url, params=params)
         return response.json() if response.status_code == 200 else None
