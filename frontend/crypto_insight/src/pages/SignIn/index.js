6318a52c2d8537627736018181433de5ef37a9de
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-11 16:13:38 -0300

    Componentes Welcome e SigIn

---
+++ frontend/crypto_insight/src/pages/SignIn/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SignIn() {
+    return (
+        <View>Tela Login</View>
+    );
+}
---
+++ frontend/crypto_insight/src/pages/Welcome/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Welcome() {
+    return (
+        <View>Tela Bem Vindo</View>
+    );
+}
