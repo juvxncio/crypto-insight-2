294609c726af63ee9ddd72218dcb3752e48ad3a2
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-12 19:23:48 -0300

    Instalação e Configuração React Navigation

--- frontend/crypto_insight_front/App.tsx
+++ frontend/crypto_insight_front/App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/pages/login';
+import { StyleSheet } from 'react-native';
+import './gesture-handler';
+import Routes from './src/routes/index.routes';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-      <Login/>
+      <NavigationContainer>
+        <Routes/>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1
+  }
 });
