ec418bea7e8479f95e7aeb1dc9170e16029cead4
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-12 19:22:45 -0300

    Tela de Login finalizada

--- frontend/crypto_insight_front/src/pages/login/index.tsx
+++ frontend/crypto_insight_front/src/pages/login/index.tsx
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
-import {Text, View, Image, TextInput, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TextInput, TouchableOpacity, Alert, ActivityIndicator} from 'react-native';
 import { style } from "./styles";
 import Logo from '../../assets/logo.png'
 import Arroba from '../../assets/arroba.png'
 import Cadeado from '../../assets/cadeado.png'
 import { themas } from "../../global/themes";
+import { InputField } from "../../components/input";
+import { Button } from "../../components/button";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
 
 export default function Login() {
+
+    const navigation = useNavigation<NavigationProp<any>>();
+
     const [email,setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function getLogin(){
+        try {
+            setLoading(true)
+            
+            if(!email || !password){
+                return Alert.alert('Atenção', 'Informe os campos obrigatórios!')
+            }
+
+            navigation.navigate('BottomRoutes')
+
+            console.log('LOGOU!')
+
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
 
     return(
         <View style={style.container}>
@@ -20,36 +46,22 @@ export default function Login() {
                 />
             </View>
             <View style={style.boxMid}>
-                <View style={style.boxInput}>
-                    <TextInput
-                        style={style.input}
-                        placeholder="Insira seu endereço de e-mail"
-                        placeholderTextColor={'#A3A3A3'}
-                        value={email}
-                        onChangeText={setEmail}
-                    />
-                    <Image
-                        source={Arroba}
-                        />
-                </View>
-                <View style={style.boxInput}>
-
-                    <TextInput
-                        style={style.input}
-                        placeholder="Digite sua senha"
-                        placeholderTextColor={'#A3A3A3'}
-                        value={password}
-                        onChangeText={setPassword}
-                        />
-                    <Image
-                        source={Cadeado}
-                        />
-                </View>
+                <InputField
+                icon={Arroba}
+                placeholder="Insira seu endereço de e-mail"
+                value={email}
+                onChangeText={setEmail}
+                />
+                <InputField
+                    icon={Cadeado}
+                    placeholder="Digite sua senha"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={true}
+                />
             </View>
             <View style={style.boxBottom}>
-                <TouchableOpacity style={style.button}>
-                    <Text style={style.textButton}>Entrar</Text>
-                </TouchableOpacity>
+                <Button title="Entrar" onPress={getLogin} loading={loading} />
             </View>
             <Text style={style.textBottom}>Ainda não possui uma conta? <Text style={{color:themas.colors.primary}}>Cadastre-se</Text></Text>
         </View>
