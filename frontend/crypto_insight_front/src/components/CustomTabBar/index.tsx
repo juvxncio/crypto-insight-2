06bba2a05e0574ce587822db20583c887fb2acef
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-13 18:51:19 -0300

    TabBar página inicial

---
+++ frontend/crypto_insight_front/src/components/CustomTabBar/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { style } from "./styles";
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { themas } from "../../global/themes";
+
+export default function CustomTabBar({ state, navigation }: BottomTabBarProps) {
+
+  const go = (screenName: string) => {
+    navigation.navigate(screenName);
+  };
+
+  return (
+    <View style={style.tabArea}>
+      <TouchableOpacity style={style.tabItem} onPress={() => go('Home')}>
+        <AntDesign
+          name="bars"
+          style={{
+            opacity: state.index === 0 ? 1 : 0.3,
+            color: themas.colors.primary,
+            fontSize: 32,
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={style.tabItem} onPress={() => go('User')}>
+        <FontAwesome
+          name="user"
+          style={{
+            opacity: state.index === 1 ? 1 : 0.3,
+            color: themas.colors.primary,
+            fontSize: 32,
+          }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ frontend/crypto_insight_front/src/components/CustomTabBar/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    tabArea:{
+        flexDirection:'row',
+        height:80,
+        justifyContent:'space-around',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+    },
+    tabItem:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+});
