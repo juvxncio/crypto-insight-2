a8eec75b776d9273e0a424e6b3b50bb8de2dc85b
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-13 18:50:01 -0300

    Criação da view de Login

--- backend/crypto_insight/cryptos/views.py
+++ backend/crypto_insight/cryptos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 from .coin_gecko import buscar_dados_criptomoedas, CoinGeckoAPI
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes, api_view
@@ -20,6 +22,27 @@ def listar_criptomoedas(request):
         return Response(
             {'erro': 'Falha ao buscar dados da API'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not email or not password:
+            return Response({"detail": "Email e senha são obrigatórios."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = refresh.access_token
+
+            return Response({
+                "access_token": str(access_token),
+                "refresh_token": str(refresh),
+            }, status=status.HTTP_200_OK)
+
+        return Response({"detail": "Credenciais inválidas."}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LogoutView(APIView):
