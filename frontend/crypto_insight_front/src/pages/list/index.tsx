3c5ff74ad5cf88917f83ed41f1070b3a94492133
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-18 19:15:33 -0300

    Início da tela inicial que mostrará as moedas

--- frontend/crypto_insight_front/src/pages/list/index.tsx
+++ frontend/crypto_insight_front/src/pages/list/index.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { style } from "./styles";
+import { Input } from "../../components/inputHome";
+import { MaterialIcons } from '@expo/vector-icons'
+import { getUserProfile } from "../../api";
 
 export default function List() {
-    return(
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Text>LISTA</Text>
+    const [username, setUsername] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function fetchUserProfile() {
+            try {
+                const data = await getUserProfile();
+                setUsername(data.username);
+            } catch (error) {
+                console.error("Erro ao carregar perfil do usuário:", error);
+            }
+        }
+
+        fetchUserProfile();
+    }, []);
+
+    return (
+        <View style={style.container}>
+            <View style={style.header}>
+                <Text style={style.greeting}>
+                    Bom dia, <Text style={{ fontWeight: 'bold' }}>{username || "Usuário"}</Text>
+                </Text>
+                <View style={style.boxInput}>
+                    <Input IconLeft={MaterialIcons} iconLeftName="search" />
+                </View>
+            </View>
+            <View style={style.boxList}>
+            </View>
         </View>
-    )
+    );
 }
