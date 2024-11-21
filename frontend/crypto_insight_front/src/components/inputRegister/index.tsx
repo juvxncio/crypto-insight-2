244757fcb6f4f051cd118f01a8b2d27b4f419c13
Gabriel Juvêncio <bieljuvencio@gmail.com>
2024-11-21 18:58:26 -0300

    Refatoração do component inputRegister

--- frontend/crypto_insight_front/src/components/inputRegister/index.tsx
+++ frontend/crypto_insight_front/src/components/inputRegister/index.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { View, TextInput, Image, TextInputProps, StyleSheet } from "react-native";
+import { View, TextInput, Image, TextInputProps } from "react-native";
 import { styles } from "./styles";
 
 interface InputFieldProps extends TextInputProps {
   icon?: any;
+  value: string;
   label?: string;
+  onChangeText: (text: string) => void;
   placeholder: string;
-  secureTextEntry?: boolean;
-  leftIcon?: React.ReactNode;
-  // value: string;
-  // onChangeText: (text: string) => void;
 }
 
-export function InputFieldRegister({ 
-  icon, 
-  label, 
-  placeholder, 
-  secureTextEntry = false, 
-  value, onChangeText,
- }: InputFieldProps) : JSX.Element {
+export function InputFieldRegister({ icon, value, label, onChangeText, placeholder, ...rest }: InputFieldProps) {
   return (
     <View style={styles.container}>
       <TextInput
@@ -27,6 +19,7 @@ export function InputFieldRegister({
         placeholderTextColor="#A3A3A3"
         value={value}
         onChangeText={onChangeText}
+        {...rest}
       />
       {icon && <Image source={icon} style={styles.icon} />}
     </View>
